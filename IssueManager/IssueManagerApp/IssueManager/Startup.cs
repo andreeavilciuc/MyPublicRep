5b43362b80489a26c36f9656e2541da090a9afb0
Andreea Vilciuc <andreea.vilciuc@gmail.com>
2020-09-04 13:43:39 +0300

    created project, created roles

---
+++ IssueManager/IssueManagerApp/IssueManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IssueManager.Startup))]
+namespace IssueManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
